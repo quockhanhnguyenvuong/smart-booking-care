14d9b832df2256feea3a3c634a39c7b39ca9e7d1
quockhanhnguyenvuong <quockhanhnv2001@gmail.com>
2023-03-22 21:01:39 +0700

    third commit

--- src/route/web.js
+++ src/route/web.js
@@ -7,6 +7,20 @@ let initWebRoutes = (app) => {
   router.get("/", homeController.getHomePage);
   router.get("/about", homeController.getAboutPage);
 
+  // API create new user
+  router.get("/crud", homeController.getCRUD);
+  router.post("/post-crud", homeController.postCRUD);
+
+  //API read user
+  router.get("/get-crud", homeController.displayGetCRUD);
+
+  //API update user
+  router.get("/edit-crud", homeController.getEditCRUD);
+  router.post("/put-crud", homeController.putCRUD);
+
+  //API delete user
+  router.get("/delete-crud", homeController.deleteCRUD);
+
   return app.use("/", router);
 };
 
